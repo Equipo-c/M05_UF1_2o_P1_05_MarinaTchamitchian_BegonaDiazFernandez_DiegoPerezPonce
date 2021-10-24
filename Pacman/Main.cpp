bda0b8ea5caf51f1dcb8d901e33c8c757a346a02
Begoña Diaz <labastidafdez@gmail.com>
2021-10-24 16:16:03 +0200

    Mapa codigo

--- Pacman/Main.cpp
+++ Pacman/Main.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
-using namespace std; 
-#define MAXFILES 20
-#define MAXCOLS 31
-
-char mapa[MAXFILES][MAXCOLS] = {
-	""
-};
+using namespace std;
 
 
+#define CONSOLE_HEIGHT 10
+#define CONSOLE_WIDTH 10
+
+//char relleno[CONSOLE_HEIGHT][CONSOLE_WIDTH]={"#"};
+char relleno =  '#' ;
+char input= ' ';
+bool run = true; 
+
+
+// While() {} numero de veces que se repite la consola
+void Input() {
+	cin >> input;
+}
+
+void Logica() {
+	//salir del juego
+	if (input == 'q' || input == 'Q')
+	{
+		run = false; 
+	}
+}
+
+void Draw() {
+	//limpiar pantalla tras interaccion
+		system("CLS");
+		
+		//int i, j; 
+	for (size_t i = 0; i < CONSOLE_HEIGHT; i++)
+	{
+		for (size_t j = 0; j < CONSOLE_WIDTH; j++)
+		{
+			cout << relleno; 
+		}
+
+	}
+	
+		
+	cout << endl; 
+}
 
 int main()
 {
-	cout << "hello World!" << endl; 
+	while (run) {
+		
+		Input();
+		Logica(); 
+		Draw(); 
+	}
+
+	
 }
+
+
+
+
