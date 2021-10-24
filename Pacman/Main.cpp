69717882254be352c115866e8afab975fee5eda9
labas <labas@DESKTOP-145F86K>
2021-10-24 13:28:50 +0200

    Agregar archivos de proyecto.

---
+++ Pacman/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+#define MAXFILES 20
+#define MAXCOLS 31
+
+char mapa[MAXFILES][MAXCOLS] = {
+	""
+};
+
+
+
+int main()
+{
+	cout << "hello World!" << endl; 
+}
